d0c46811861b0510ab6dbf5e8881b05f3f4773b1
Gennadii Remizov <gremov@gmail.com>
2016-10-07 16:33:21 +0300

    lider caption fixed

--- Projects/tasks/animates/js/opacity.js
+++ Projects/tasks/animates/js/opacity.js
@@ -9,20 +9,20 @@ $('.slider-image-overlay-left').click(function() {
 });
 
 $('.slider-image-overlay-right').click(function() {
-    // $( ".slider-caption" ).animate({
-    //     opacity: 1,
-    //     duration: "4000",
-    // });
+    $( ".slider-caption" ).animate({
+        opacity: 1,
+        duration: "4000",
+    });
 
-    $(".slider-image-overlay-right").toggle(function() {
-            // Отображаем скрытый блок
-            $("DIV#box").fadeIn(); // fadeIn - плавное появление
-            return false; // не производить переход по ссылке
-        },
-        function() {
-            // Скрываем блок
-            $("DIV#box").fadeOut(); // fadeOut - плавное исчезновение
-            return false; // не производить переход по ссылке
-        }); // end of toggle()
-}); // end of ready()
+//     $(".slider-image-overlay-right").toggle(function() {
+//             // Отображаем скрытый блок
+//             $(".slider-captionbox").fadeIn(); // fadeIn - плавное появление
+//             return false; // не производить переход по ссылке
+//         },
+//         function() {
+//             // Скрываем блок
+//             $(".slider-caption").fadeOut(); // fadeOut - плавное исчезновение
+//             return false; // не производить переход по ссылке
+//         }); // end of toggle()
+// }); // end of ready()
 });
