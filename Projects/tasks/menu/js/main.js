d07d26eeec0aed912174902f62a4149d5d04e23c
Gennadii Remizov <gremov@gmail.com>
2016-10-17 17:17:42 +0300

    icons and some animate scenario fixed

--- Projects/tasks/menu/js/main.js
+++ Projects/tasks/menu/js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    // var lanRus = $('.langs-item-rus');
-    //     lanUkr = $('.langs-item-ukr');
-    //     lanEng = $('.langs-item-eng');
     var menuOverlay = $('.nav-bar');
         langsOpen = $('.lan');
         langsMain = $('.lan-wrap');
@@ -15,27 +12,21 @@ $(document).ready(function(){
         services = $('.menu-item:nth-child(4)');
         pressa = $('.menu-item:nth-child(5)');
         contacts = $('.menu-item:last-child');
-        socmedia = $('.network-list');
+        socmedia = $('.icon__list');
 
-    $('.hamburger-icon').click(function(){
-        // $(".hamburger-icon").find('.icon-inner').unbind("mouseenter mouseleave");
+    $('.hamburger-line').click(function(){
+
         $(this).toggleClass('open');
 
+        // $( ".hamburger-line.open + span + .hamburger-open" );
+        //
+        // $( ".hamburger-line.start + span + .hamburger-close" );
     });
-
-    // menu.click(function() {
-    //     $( ".hamburger span" ).toggle();
-    // });
-
-    // menu.click(function() {
-    //     $( ".hamburger span" ).toggle();
-    //     // $( ".langs-wrap" ).css("display", "none");
-    // });
 
     var tl = new TimelineMax({paused:true, reversed:true});
     // tl.to([lanRus, lanUkr, lanEng], 1,  {left: 0, opacity: 1});
     tl.to(textMenu, 0.1, {opacity: 0, ease:Back.easeInOut});
-    tl.to(textClose, 0.1, {opacity: 1, ease:Back.easeInOut});
+    // tl.to(textClose, 0.1, {opacity: 1, ease:Back.easeIn}, 1);
     tl.to(menuOverlay, 0.75, {left: 0, opacity: 1, ease:Power2.easeInOut});
     tl.to(langsMain, 1, {autoAlpha: 0, display: 'none', left: 0, ease:Back.easeInOut}, '-=0.9');
     tl.to(content, 1, {autoAlpha: 0, display: 'none', left: 0, ease:Back.easeInOut}, '-=1.2');
@@ -46,20 +37,10 @@ $(document).ready(function(){
     tl.to(services, 1.5, {top: 50, opacity: 1, ease:Power2.easeInOut}, 1.25);
     tl.to(pressa, 1.5, {top: 50, opacity: 1, ease:Power2.easeInOut}, 1.5);
     tl.to(contacts, 1.5, {top: 50, opacity: 1, ease:Power2.easeInOut}, 2.5, 'menu');
-    // tl.to(logo, 1, {left: 0, opacity: 1, ease:Power2.easeInOut}, 1.5);
     tl.to(socmedia, 0.25, {left: -15, opacity: 1, ease:Power2.easeInOut});
 
 
-    $('.hamburger-icon').click(function () {
+    $('.hamburger-line').click(function () {
         tl.reversed() ? tl.play() : tl.reverse();
     });
-    // tl.to(socmedia, 0.5, {left: 0, opacity: 1}, '+=1.2');
-    // tl.to([main, portfolio, about, services, pressa, contacts], 3, {top: 100, opacity: 1}, 1);
-
-    // tl.addCallBack(function() {
-    //     tl.to(socmedia, 1, {left: 100, opacity: 1});
-    // }, 'menu');
-
-
-
 });
